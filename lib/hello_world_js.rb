eaaf9862ae9d361a721b16942874a6d9702d4b12
jinta-shimo02 <jinjinnbaaahi.0514@icloud.com>
2025-03-23 19:34:09 +0900

    first commit

---
+++ lib/hello_world_js.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "hello_world_js/version"
+
+module HelloWorldJs
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/hello_world_js/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module HelloWorldJs
+  VERSION = "0.1.0"
+end
